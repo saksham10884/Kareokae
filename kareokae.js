adb035230719173f1ef4af50f461b9cc8d935c7f
saksham10884 <sakshamsharma25106@gmail.com>
2025-05-27 01:09:05 +0530

    kareokae.js

--- kareokae.js
+++ kareokae.js
@@ -140,11 +140,8 @@ async function writesongs(link){
     })
     previous.addEventListener("click",()=>{
         let index=0
-        console.log(audio.src)
-        console.log(songsUl)
         let src=audio.src.replace("songs","kareokae")
         index=songsUl.indexOf(src)
-        console.log(index)
         if(index>0){
             src=songsUl[index-1]
         }
@@ -160,11 +157,8 @@ async function writesongs(link){
     })
     next.addEventListener("click",()=>{
         let index=0
-        console.log(audio.src)
-        console.log(songsUl)
         let src=audio.src.replace("songs","kareokae")
         index=songsUl.indexOf(src)
-        console.log(index)
         if(index< songsUl.length-1){
             src=songsUl[index+1]
         }
